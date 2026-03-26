e9f3849bbc2432e38512072a27be523734f0ff87
Jonas Kaufmann <jkaufman@mpi-sws.org>
2026-03-26 12:58:01 +0000

    src_simbricks/rules.mk: use lld-20 as linker
    This was required to get the address sanitizer to work.

--- src_simbricks/rules.mk
+++ src_simbricks/rules.mk
@@ -9,6 +9,7 @@ ADDITIONAL_CFLAGS ?=
 ADDITIONAL_VFLAGS ?=
 BASE_CFLAGS := -I$(abspath $(lib_dir)) -std=c++17 $(ADDITIONAL_CFLAGS)
 BASE_VFLAGS := -I$(abspath $(d)) -y $(JPEG_SRC_V) -y $(JPEG_AXI_RTL) -y $(JPEG_AXIS_RTL) -Wno-fatal --threads 1 -j `nproc` -O3 --compiler clang -MAKEFLAGS "OPT=-march=native" -Wno-TIMESCALEMOD -Wno-WIDTHTRUNC -Wno-WIDTHEXPAND --Mdir $(VERILATOR_MDIR) --cc --build --exe
+BASE_LDFLAGS := -fuse-ld=lld-20
 
 NO_TRACE_CFLAGS := $(BASE_CFLAGS) $(ADDITIONAL_CFLAGS) -DTRACE_MODE=0
 NO_TRACE_VFLAGS := $(BASE_VFLAGS) $(ADDITIONAL_VFLAGS)
@@ -26,23 +27,26 @@ jpeg-all: $(BIN)
 $(d)jpeg_rtl_sim_no_trace: CC := clang-20
 $(d)jpeg_rtl_sim_no_trace: CXX := clang++-20
 $(d)jpeg_rtl_sim_no_trace: private CFLAGS := $(NO_TRACE_CFLAGS)
+$(d)jpeg_rtl_sim_no_trace: private LDFLAGS := $(BASE_LDFLAGS)
 $(d)jpeg_rtl_sim_no_trace: private VFLAGS := $(NO_TRACE_VFLAGS)
 $(d)jpeg_rtl_sim_no_trace: $(d)jpeg_sim_rtl.sv $(d)jpeg_rtl_1.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" $(VFLAGS)  -o $(abspath $@) $^
+	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" -LDFLAGS "$(LDFLAGS)" $(VFLAGS)  -o $(abspath $@) $^
 
 $(d)jpeg_rtl_sim_vcd_trace: CC := clang-20
 $(d)jpeg_rtl_sim_vcd_trace: CXX := clang++-20
 $(d)jpeg_rtl_sim_vcd_trace: private CFLAGS := $(VCD_TRACE_CFLAGS)
+$(d)jpeg_rtl_sim_vcd_trace: private LDFLAGS := $(BASE_LDFLAGS)
 $(d)jpeg_rtl_sim_vcd_trace: private VFLAGS := $(VCD_TRACE_VFLAGS)
 $(d)jpeg_rtl_sim_vcd_trace: $(d)jpeg_sim_rtl.sv $(d)jpeg_rtl_1.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" $(VFLAGS) -o $(abspath $@) $^
+	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" -LDFLAGS "$(LDFLAGS)" $(VFLAGS) -o $(abspath $@) $^
 
 $(d)jpeg_rtl_sim_saif_trace: CC := clang-20
 $(d)jpeg_rtl_sim_saif_trace: CXX := clang++-20
 $(d)jpeg_rtl_sim_saif_trace: private CFLAGS := $(SAIF_TRACE_CFLAGS)
+$(d)jpeg_rtl_sim_saif_trace: private LDFLAGS := $(BASE_LDFLAGS)
 $(d)jpeg_rtl_sim_saif_trace: private VFLAGS := $(SAIF_TRACE_VFLAGS)
 $(d)jpeg_rtl_sim_saif_trace: $(d)jpeg_sim_rtl.sv $(d)jpeg_rtl_1.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" $(VFLAGS) -o $(abspath $@) $^
+	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" -LDFLAGS "$(LDFLAGS)" $(VFLAGS) -o $(abspath $@) $^
 
 jpeg-clean:
 	rm -rf $(BIN) $(VERILATOR_MDIR)
