b2f89460a5ffba9bfd838a97295255bdf823804d
Jonas Kaufmann <jkaufman@mpi-sws.org>
2026-04-14 14:55:26 +0000

    src_simbricks/rules.mk: add synthesized variant

--- src_simbricks/rules.mk
+++ src_simbricks/rules.mk
@@ -14,6 +14,7 @@ BASE_LDFLAGS := -fuse-ld=lld-20
 
 CFLAGS := $(BASE_CFLAGS) $(ADDITIONAL_CFLAGS)
 VFLAGS_RTL := $(BASE_VFLAGS) -y $(JPEG_AXI_RTL) -y $(JPEG_AXIS_RTL) $(ADDITIONAL_VFLAGS)
+VFLAGS_SYNTH := $(BASE_VFLAGS) $(ADDITIONAL_VFLAGS)
 
 NO_TRACE_CFLAGS := -DTRACE_MODE=0
 NO_TRACE_VFLAGS :=
@@ -24,7 +25,10 @@ SAIF_TRACE_VFLAGS := --trace-saif --no-trace-top
 
 BIN := $(d)jpeg_rtl_sim_no_trace \
 	$(d)jpeg_rtl_sim_vcd_trace \
-	$(d)jpeg_rtl_sim_saif_trace
+	$(d)jpeg_rtl_sim_saif_trace \
+	$(d)jpeg_synth_sim_no_trace \
+	$(d)jpeg_synth_sim_vcd_trace \
+	$(d)jpeg_synth_sim_saif_trace
 
 jpeg-all: $(BIN)
 
@@ -50,6 +54,30 @@ $(d)jpeg_rtl_sim_saif_trace: private CFLAGS := $(CFLAGS) $(SAIF_TRACE_CFLAGS)
 $(d)jpeg_rtl_sim_saif_trace: private LDFLAGS := $(BASE_LDFLAGS)
 $(d)jpeg_rtl_sim_saif_trace: private VFLAGS := $(VFLAGS_RTL) $(SAIF_TRACE_VFLAGS)
 $(d)jpeg_rtl_sim_saif_trace: $(d)jpeg_sim_rtl.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
+	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" -LDFLAGS "$(LDFLAGS)" $(VFLAGS) -o $(abspath $@) $^
+
+$(d)jpeg_synth_sim_no_trace: CC := clang-20
+$(d)jpeg_synth_sim_no_trace: CXX := clang++-20
+$(d)jpeg_synth_sim_no_trace: private CFLAGS := $(CFLAGS) $(NO_TRACE_CFLAGS)
+$(d)jpeg_synth_sim_no_trace: private LDFLAGS := $(BASE_LDFLAGS)
+$(d)jpeg_synth_sim_no_trace: private VFLAGS := $(VFLAGS_SYNTH) $(NO_TRACE_VFLAGS)
+$(d)jpeg_synth_sim_no_trace: $(d)jpeg_sim_rtl.sv $(d)jpeg_top_bd_synth.v $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
+	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" -LDFLAGS "$(LDFLAGS)" $(VFLAGS) -o $(abspath $@) $^
+
+$(d)jpeg_synth_sim_vcd_trace: CC := clang-20
+$(d)jpeg_synth_sim_vcd_trace: CXX := clang++-20
+$(d)jpeg_synth_sim_vcd_trace: private CFLAGS := $(CFLAGS) $(VCD_TRACE_CFLAGS)
+$(d)jpeg_synth_sim_vcd_trace: private LDFLAGS := $(BASE_LDFLAGS)
+$(d)jpeg_synth_sim_vcd_trace: private VFLAGS := $(VFLAGS_SYNTH) $(VCD_TRACE_VFLAGS)
+$(d)jpeg_synth_sim_vcd_trace: $(d)jpeg_sim_rtl.sv $(d)jpeg_top_bd_synth.v $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
+	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" -LDFLAGS "$(LDFLAGS)" $(VFLAGS) -o $(abspath $@) $^
+
+$(d)jpeg_synth_sim_saif_trace: CC := clang-20
+$(d)jpeg_synth_sim_saif_trace: CXX := clang++-20
+$(d)jpeg_synth_sim_saif_trace: private CFLAGS := $(CFLAGS) $(SAIF_TRACE_CFLAGS)
+$(d)jpeg_synth_sim_saif_trace: private LDFLAGS := $(BASE_LDFLAGS)
+$(d)jpeg_synth_sim_saif_trace: private VFLAGS := $(VFLAGS_SYNTH) $(SAIF_TRACE_VFLAGS)
+$(d)jpeg_synth_sim_saif_trace: $(d)jpeg_sim_rtl.sv $(d)jpeg_top_bd_synth.v $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
 	verilator --top-module jpeg_sim -CFLAGS "$(CFLAGS)" -LDFLAGS "$(LDFLAGS)" $(VFLAGS) -o $(abspath $@) $^
 
 jpeg-clean:
